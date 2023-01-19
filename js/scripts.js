64e35c37c35d9a0696bde557380c5adbceb3ecc6
James Provance <jb24x7@gmail.com>
2023-01-19 13:10:49 -0800

    add consonants and vowel changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,67 @@
-let form = document.querySelector("form");
-let p = document.getElementById("results");
+// let form = document.querySelector("form");
+// let p = document.getElementById("results");
 
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-  let number1 = Number(document.querySelector("input#number1").value);
-  let number2 = Number(document.querySelector("input#number2").value);
-  let results = [];
-  p.innerHTML = "";
+// form.addEventListener("submit", function(event) {
+//   event.preventDefault();
+//   let number1 = Number(document.querySelector("input#number1").value);
+//   let number2 = Number(document.querySelector("input#number2").value);
+//   let results = [];
+//   p.innerHTML = "";
 
-  // console.log(number1)
-  if (number1 % number2 === 0) {
-    for (let index = number2; index <= number1; index+= number2) {
-      results.push(index)
-      console.log(index)
-    } 
-    p.append(results.join(", "))
-  }
-
-
+//   // console.log(number1)
+//   if (number1 % number2 === 0) {
+//     for (let index = number2; index <= number1; index+= number2) {
+//       results.push(index)
+//       console.log(index)
+//     } 
+//     p.append(results.join(", "))
+//   }
 
 
 
 
-});
+
+
+// });
+
+let form2 = document.getElementById('form');
+
+function pigLatin(text) {
+  
+  const vowels = ['a','e','i','o','u'];
+  const usrTxt = text.split(' ');
+  const output = [];
+
+  usrTxt.forEach((word) => {
+    
+    if (vowels.includes(word[0])) {
+      
+
+      output.push(word.concat("way"));
+
+    } else if (!vowels.includes(word[0])) {
+  
+      let cons = "";
+      let end = "";
+      let count = 0;
+
+      for (let i=0; !vowels.includes(word[i]); i++) {
+        
+        count++;
+        cons += word[i];
+        console.log(cons)
+      }
+
+      end += (word.slice(count++));
+      output.push(end + cons + 'ay')
+
+
+    
+
+    }
+    console.log(output)
+    });
+  
+}
+
+//form2.addEventListener("submit", )
