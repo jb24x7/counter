ad764495ce6cb956b2aec37dddeb75502c90cb2c
James Provance <jb24x7@gmail.com>
2023-01-19 16:39:39 -0800

    add regular expressions

--- js/scripts.js
+++ js/scripts.js
@@ -22,67 +22,98 @@
 
 
 
-// });
+// // });
 
-let form2 = document.getElementById('form');
+// let form2 = document.getElementById('form');
+// let p = document.getElementById('results')
+// let userInput = document.getElementById('sentence')
+
+// function pigLatin(text) {
+
+//   const vowels = ['a', 'e', 'i', 'o', 'u'];
+//   const usrTxt = text.split(' ');
+//   const output = [];
+
+//   usrTxt.forEach((word) => {
+
+//     if (vowels.includes(word[0])) {
+
+
+//       output.push(word.concat("way"));
+
+//     } else if (!vowels.includes(word[0])) {
+
+//       let cons = "";
+//       let end = "";
+//       let count = 0;
+
+//       if (word[0] === 'q' && word[1] === 'u') {
+
+//         count = 2;
+//         cons = 'qu';
+
+//       } else {
+
+//         for (let i = 0; !vowels.includes(word[i]); i++) {
+
+//           if (word[i] === 'q') {
+
+//             break;
+
+//           } else {
+
+//             count++;
+//             cons += word[i];
+
+//           }
+//         }
+//       }
+
+//       end += (word.slice(count++));
+//       output.push(end + cons + 'ay')
+
+//     }
+
+//   });
+//   return output
+
+// }
+
+// form2.addEventListener("submit", function (event) {
+//   let p = document.getElementById('results')
+//   let userInput = document.getElementById('sentence').value
+//   console.log(userInput)
+//   event.preventDefault();
+//   p.append(pigLatin(userInput).join(' '))
+// })
+
+
+
+// let form2 = document.getElementById('form');
 // let p = document.getElementById('results')
 // let userInput = document.getElementById('sentence')
 
 function pigLatin(text) {
 
-  const vowels = ['a', 'e', 'i', 'o', 'u'];
   const usrTxt = text.split(' ');
   const output = [];
 
-  usrTxt.forEach((word) => {
+  let vowelArr = usrTxt.filter(words => words.match(/\b[aeiou]\w*\b/gi)).map(word => word + "way");
 
-    if (vowels.includes(word[0])) {
-
-
-      output.push(word.concat("way"));
-
-    } else if (!vowels.includes(word[0])) {
-
-      let cons = "";
-      let end = "";
-      let count = 0;
-
-      if (word[0] === 'q' && word[1] === 'u') {
-
-        count = 2;
-        cons = 'qu';
-
-      } else {
-
-        for (let i = 0; !vowels.includes(word[i]); i++) {
-
-          if (word[i] === 'q') {
-
-            break;
-
-          } else {
-
-            count++;
-            cons += word[i];
-
-          }
-        }
-      }
-
-      end += (word.slice(count++));
-      output.push(end + cons + 'ay')
-
-    }
-
+  let consArr = usrTxt.filter(words => words.match(/\b[^qaeiou]+(q|[aeiou])\w*\b/gi)).map(word => {
+    return word.replace(/\b[^qaeiou]+/gi, "")
   });
-  return output
+
+  
+
+  console.log(consArr)
 
 }
 
-form2.addEventListener("submit", function (event) {
-  let p = document.getElementById('results')
-  let userInput = document.getElementById('sentence').value
-  console.log(userInput)
-  event.preventDefault();
-  p.append(pigLatin(userInput).join(' '))
-})
+// form2.addEventListener("submit", function (event) {
+//   let p = document.getElementById('results')
+//   let userInput = document.getElementById('sentence').value
+  
+//   event.preventDefault();
+//   p.append(pigLatin(userInput).join(' '))
+// })
